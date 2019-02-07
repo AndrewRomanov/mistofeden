a0bb9998c9863b5f0d5f54ca98e998703c8bd63a
Roman Kazakov <roman@roman.ru>
2019-02-07 22:41:00 +0400

    Добавить меню паузы игры

---
+++ MistOfEden/Assets/Scripts/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string mainMenuScene;
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ResumeGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = !isPaused;
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+
+    public void ReturnToMain()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(mainMenuScene);
+    }
+}
