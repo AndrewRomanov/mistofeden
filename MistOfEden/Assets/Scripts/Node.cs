604b89e2c5b79af5ee2f6657cb173a8133ff3473
Andrew Romanov <wesemeus@gmail.com>
2019-02-07 22:18:58 +0400

    Поиск оптимального пути между точками

--- MistOfEden/Assets/Scripts/Node.cs
+++ MistOfEden/Assets/Scripts/Node.cs
@@ -16,13 +16,39 @@ public class Node
     public Vector3 WorldPosition;
 
     /// <summary>
+    /// Местоположение клетки в массиве
+    /// </summary>
+    public int GridX, GridY;
+
+    /// <summary>
+    /// "Вес" или "стоимость" клетки.
+    /// GCost - расстояние до стартовой клетки
+    /// HCost - расстояние до конечной клетки
+    /// </summary>
+    public int GCost, HCost;
+
+    /// <summary>
+    /// Предыдущая клетка, по которой был проложен путь
+    /// </summary>
+    public Node parent;
+
+    /// <summary>
     /// Конструктор класса клетки
     /// </summary>
     /// <param name="walkable">Возможно ли ходить по данной клетке</param>
     /// <param name="worldPosition">Позиция клетки на поле</param>
-    public Node(bool walkable, Vector3 worldPosition)
+    /// <param name="gridX">Позиция клетки в массиве по X</param>
+    /// <param name="gridY">Позиция клетки в массиве по Y</param>
+    public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
     {
         Walkable = walkable;
         WorldPosition = worldPosition;
+        GridX = gridX;
+        GridY = gridY;
     }
+
+    /// <summary>
+    /// Общий "вес" или "стоимость" клетки. Равна сумме GCost и HCost
+    /// </summary>
+    public int FCost => GCost + HCost;
 }
---
+++ MistOfEden/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Класс, отвечающий за поиск пути
+/// </summary>
+public class Pathfinding : MonoBehaviour
+{
+    /// <summary>
+    /// Поля определяющие начальную и конечную точки
+    /// </summary>
+    public Transform Seeker, Target;
+
+    /// <summary>
+    /// Игровая сетка
+    /// </summary>
+    private GridScript grid;
+
+    void Awake()
+    {
+        grid = GetComponent<GridScript>();
+    }
+
+    void Update()
+    {
+        // Ищем путь
+        FindPath(Seeker.position, Target.position);
+    }
+
+    /// <summary>
+    /// Метод нахождения оптимального пути
+    /// </summary>
+    /// <param name="startPos">Стартовая точка</param>
+    /// <param name="targetPos">Конечная точка</param>
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        // Получаем ноды стартовой и конечной точки
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>(); // Список нодов на рассмотрение
+        HashSet<Node> closedSet = new HashSet<Node>(); // Список просмотренных нодов
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node node = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if ((openSet[i].FCost < node.FCost || openSet[i].FCost == node.FCost) && openSet[i].HCost < node.HCost)
+                {
+                    node = openSet[i];
+                }
+            }
+
+            openSet.Remove(node);
+            closedSet.Add(node);
+
+            if (node == targetNode)
+            {
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            foreach (Node neighbour in grid.GetNeighbours(node))
+            {
+                if (!neighbour.Walkable || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+
+                int newCostToNeighbour = node.GCost + GetDistance(node, neighbour);
+                if (newCostToNeighbour < neighbour.GCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.GCost = newCostToNeighbour;
+                    neighbour.HCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = node;
+
+                    if (!openSet.Contains(neighbour))
+                    {
+                        openSet.Add(neighbour);
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Метод, формирующий путь
+    /// </summary>
+    /// <param name="startNode">Стартовый нод</param>
+    /// <param name="endNode">Конечный нод</param>
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while (currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+
+        grid.path = path;
+    }
+
+    /// <summary>
+    /// Метод, считающий расстояние между нодами
+    /// </summary>
+    /// <param name="nodeA">Первый нод</param>
+    /// <param name="nodeB">Второй нод</param>
+    /// <returns></returns>
+    int GetDistance(Node nodeA, Node nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.GridX - nodeB.GridX);
+        int dstY = Mathf.Abs(nodeA.GridY - nodeB.GridY);
+
+        if (dstX > dstY)
+        {
+            return 14 * dstY + 10 * (dstX - dstY);
+        }
+        return 14 * dstX + 10 * (dstY - dstX);
+    }
+}
